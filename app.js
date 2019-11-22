f0c7f11cbc0ecd689c20e45b2539971431cf21bc
C K Siew <ck_siew@hotmail.com>
2019-11-22 16:54:09 +0800

    Completed

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const feedRoutes = require('./routes/feed');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded
+app.use(bodyParser.json()); // application/json
+// app.use((req,res,next)=>{
+//     res.setHeader('Access-Control-Allow-Origin','*');
+//     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+//     res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+//     next();
+// })
+app.use(cors(
+
+));
+app.use('/feed',feedRoutes);
+
+app.listen(8080,()=>{
+    console.log('Connected.');
+});
