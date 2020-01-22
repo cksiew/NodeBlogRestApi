557179164870bb0f72aea7cc1b2b84ff1852ef00
C K Siew <ck_siew@hotmail.com>
2020-01-22 09:45:02 +0800

    GraphQL

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
-const express = require('express');
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
 const MONGODB_URL = process.env.MONGODB_BLOG_URL;
-const multer = require('multer');
- 
+const multer = require("multer");
+const graphqlHttp = require("express-graphql");
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
+const auth = require("./middleware/auth");
+const fs = require("fs");
+const { clearImage } = require('./util/file');
+
 const app = express();
 
 const fileStorage = multer.diskStorage({
-    destination: (req,file,cb)=>{
-        cb(null,'images')
-    },
-    filename:(req,file,cb)=>{
-        cb(null,Date.now().toString() + '-' + file.originalname)
-    }
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now().toString() + "-" + file.originalname);
+  }
 });
 
-const fileFilter = (req,file,cb)=>{
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg')
-    {
-        cb(null,true);
-    } else {
-        cb(null,false);
-    }
-}
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded
 app.use(bodyParser.json()); // application/json
@@ -36,31 +43,63 @@ app.use(bodyParser.json()); // application/json
 //     res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
 //     next();
 // })
-app.use(multer({storage:fileStorage, fileFilter:fileFilter}).single('image'));
-app.use('/images',express.static(path.join(__dirname,'images')));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(cors());
-app.use('/feed',feedRoutes);
-app.use('/auth',authRoutes);
 
-app.use((err,req,res,next)=>{
-    console.log(err);
-    const status = err.statusCode || 500;
-    const message = err.message;
-    const data = err.data;
-    res.status(status).json({message:message, data: data });
-})
+app.use(auth);
 
-mongoose.connect(MONGODB_URL)
-.then(result=>{
-    const server = app.listen(8080,()=>{
-        console.log('Connected.');
-    });
-    const io = require('./socket').init(server);
-    io.on('connection', socket=>{
-        console.log('Client connected');
-    });
-})
-.catch(err=>{
-    console.log(err);
+app.put("/post-image", (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error("Not authenticated!");
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: "No file provided!" });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: "File stored.", filePath: req.file.path });
 });
+
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || "An error occurred.";
+      const code = err.originalError.code || 500;
+      return { message: message, status: code, data: data };
+    }
+  })
+);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  const status = err.statusCode || 500;
+  const message = err.message;
+  const data = err.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(result => {
+    app.listen(8080, () => {
+      console.log("Connected.");
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
